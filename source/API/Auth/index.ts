9ef0f5c37a9ff061e39edced4615612d2e539d8f
Nikita Bersenev <bersenoff@bk.ru>
2019-02-24 20:57:52 +0800

    Написал метод регистрации

--- source/API/Auth/index.ts
+++ source/API/Auth/index.ts
@@ -3,6 +3,7 @@
  */
 
 import { Main } from "../../Classes";
+import bcrypt from "bcrypt";
 
 export default class Auth extends Main {
   constructor() {
@@ -12,9 +13,59 @@ export default class Auth extends Main {
   /**
    * @description Регистрация
    */
-  public signup(params: any) {
-    const data: string = "Hello, world!";
+  public async signup({
+    name,
+    phone,
+    email,
+    password
+  }: {
+    name: string,
+    phone: string,
+    email: string,
+    password: string
+  }) {
+    // Валидация данных
+    const phone_check = /^[0-9]{11}$/;
+    const email_check = /^[-._a-z0-9]+@(?:[a-z0-9][-a-z0-9]+\.)+[a-z]{2,6}$/i;
 
-    return data;
+    if (!phone_check.test(phone)) throw new Error("Номер телефона должен состоять из 11 цифр");
+    if (!email_check.test(email)) throw new Error("Некорректный email");
+    if (password.length < 6) throw new Error("В пароле должно быть не менее 6 символов");
+
+    const passwordHash = await bcrypt.hash(password, 10);
+
+    const result: any = await this.user.findOrCreate({
+      where: {
+        [this.Sequelize.Op.or]: [
+          { phone },
+          { email }
+        ]
+      },
+      defaults: {
+        name,
+        phone,
+        email,
+        password: passwordHash
+      }
+     });
+
+    const user = result[0];
+    const created = result[1];
+
+    if (!created) {
+      if (user.phone === phone) throw new Error("Такой номер телефона уже зарегистрирован");
+      if (user.email === email) throw new Error("Такой email уже зарегистрирован");
+    }
+
+    const verifiable_fields = ["phone", "email"];
+
+    for (let field of verifiable_fields) {
+      this.confirmation.create({
+        fk_user_id: user.user_id,
+        field
+      });
+    }
+
+    return true;
   }
 }
--- source/API/Auth/routes/index.ts
+++ source/API/Auth/routes/index.ts
@@ -1,7 +1,12 @@
 import { route } from "../../../Utils";
 import { Auth } from "../../";
 
-const signup = route("/auth/signup", (params: any) => Auth.signup(params));
+const signup = route("/auth/signup", (params: any) => Auth.signup(params), [
+  "name",
+  "phone",
+  "email",
+  "password"
+]);
 
 export default [
   signup
